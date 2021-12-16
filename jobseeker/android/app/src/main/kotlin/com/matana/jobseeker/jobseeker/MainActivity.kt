cf5df0967a7a592d79c7d8bbf52e5b282f643909
kennethliem <kennethliem991@gmail.com>
2021-12-16 12:52:56 +0700

    ok

---
+++ jobseeker/android/app/src/main/kotlin/com/matana/jobseeker/jobseeker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.matana.jobseeker.jobseeker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ jobseeker/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const DarkGrey = Color(0xffAAAAAA);
+const LightGrey = Color(0xffF0F0F0);
+
+const double BasePadding = 4.0;
+const double SafePadding = 16.0;
